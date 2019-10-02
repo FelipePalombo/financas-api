a1418212e545af5c408b401727e4fadadabd3c90
Felipe R. Palombo <feliperibeiro.rp@gmail.com>
2019-10-02 20:03:43 -0300

    Preparando a inversão de controle.

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using IoC;
 
 namespace FinancasProj
 {
@@ -26,6 +27,8 @@ namespace FinancasProj
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            IoCGeral.ConfigurarRepositorio(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
